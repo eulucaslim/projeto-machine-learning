1c3c1c088fcf2318c174019ad16ce58de0ea3cf9
Lucas Lima <lucaslimacnt14@gmail.com>
2026-05-19 23:50:59 -0400

    feat: Atualizei a base de dados e a remoção de colunas nao utilizadas

--- SUMMARY.md
+++ SUMMARY.md
@@ -9,3 +9,8 @@ uma definição bem simples e correta seria que uma feature é uma variável usa
 A feature é a informação de entrada e o target é o que eu quero prever, nesse caso a feature são os dados de umidade, pressão,
 temperatura e vento e o Target é se vai chover amanhã. 
 ```
+
+## Por que foram removidas as colunas que possuiam as informação de Min/Max do Dataset
+```
+Pelo fato de se tratar da mesma informação passada anteriormente, ou seja, não são fenômenos novos e sim derivações diretas de váriaveis que já temos e isso causa um fênomeno chamado de Multicolinearidade.
+``` 
--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import pandas as pd
 
+print("Dataset do INMET com dados meteorológicos de Manaus\n")
 # Carregamento de dados para um Dataframe
 df = pd.read_csv('dados.csv', sep=';')
-print(df.describe)
+# Exibe o começo do Dataframe para uma listagem simples dos dados
+print(df.head)
+
+# Remove as colunas listadas abaixo com seus respectivos motivos
+df_filtrado = df.drop(
+    columns=[
+        # Por se tratar de dados operacionais
+        'TEMPERATURA DA CPU DA ESTACAO(°C)',  
+        'TENSAO DA BATERIA DA ESTACAO(V)',
+
+        # Informações duplicadas - redundância informacional
+        'PRESSAO ATMOSFERICA MAX.NA HORA ANT. (AUT)(mB)',
+        'PRESSAO ATMOSFERICA MIN. NA HORA ANT. (AUT)(mB)',
+        'TEMPERATURA MAXIMA NA HORA ANT. (AUT)(°C)',
+        'TEMPERATURA MINIMA NA HORA ANT. (AUT)(°C)',
+        'TEMPERATURA ORVALHO MAX. NA HORA ANT. (AUT)(°C)',
+        'TEMPERATURA ORVALHO MIN. NA HORA ANT. (AUT)(°C)',
+        'UMIDADE REL. MAX. NA HORA ANT. (AUT)(%)',
+        'UMIDADE REL. MIN. NA HORA ANT. (AUT)(%)'
+    ]
+)
+
+print("=" * 60, "\n")
+print("Essas foram as colunas restantes: \n")
+for col in df_filtrado.columns:
+    print(f"- {col}")
+
+print("\nQuantidade de colunas restantes: ", len(df_filtrado.columns))
+print("=" * 60, "\n")
+
